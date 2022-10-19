088e00a3f163f3fcafadece28513b321d0785fb8
Chris Lee <christopher.lee222@yahoo.com>
2022-10-19 15:16:53 -0500

    start project

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+
+const App = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
