9b9599cf931c82f20e74680f468031e1f8829454
Chris Lee <christopher.lee222@yahoo.com>
2022-11-02 18:05:46 -0500

    can upload from write page

--- server/index.js
+++ server/index.js
@@ -1,25 +1,42 @@
-import express from 'express';
+import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import cookieParser from 'cookie-parser';
-import authRoutes from './routes/authRoutes.js';
-import userRoutes from './routes/usersRoutes.js';
-import postRoutes from './routes/postsRoutes.js';
+import cookieParser from "cookie-parser";
+import multer from "multer";
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/usersRoutes.js";
+import postRoutes from "./routes/postsRoutes.js";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
-
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/posts', postRoutes);
+// for uploading images
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "../client/public/upload");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + file.originalname);
+  },
+});
 
+const upload = multer({ storage });
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
+
+//my routes
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 app.listen(port, () => {
-    console.log(`Connected to the server on port ${port}. BOOM BABY!!`);
-})
+  console.log(`Connected to the server on port ${port}. BOOM BABY!!`);
+});
